76e01216bd0a30a5e07007e4127602620acbc808
Alessandro0003 <alessandroquinta3@gmail.com>
2024-05-19 22:53:43 -0300

    :construction: feat: add product to page static and settings Fallback SSG

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,23 +1,39 @@
+import { stripe } from '@/src/lib/stripe'
 import { ImageContainer, ProductContainer, ProductDetails } from '@/src/styles/pages/product'
+import { GetServerSideProps, GetStaticPaths, GetStaticProps } from 'next'
+import Image from 'next/image'
 import { useRouter } from 'next/router'
+import Stripe from 'stripe'
 
-export default function Product() {
-    const { query } = useRouter()
+interface ProductProps {
+    product: {
+      id: string
+      name: string
+      imageUrl: string
+      price: string
+      description: string
+    }
+  }
+
+export default function Product({ product }: ProductProps) {
+    // console.log(product)
+
+    const { isFallback } = useRouter()
+
+    if (isFallback) {
+        return <p>Loading...</p>
+    }
 
     return (
        <ProductContainer>
         <ImageContainer>
-            <p>Image</p>
+            <Image src={product.imageUrl} width={520} height={480} alt="" />
         </ImageContainer>
 
         <ProductDetails>
-            <h1>Camiseta X</h1>
-            <span>R$ 79,90</span>
-            <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-                Aut quibusdam aperiam ad ea sunt voluptates officia corporis 
-                eligendi aliquam quam mollitia ab minus iure dolorum blanditiis asperiores eius, unde dolorem!
-            </p>
+            <h1>{product.name}</h1>
+            <span>{product.price}</span>
+            <p>{product.description}</p>
 
             <button>
                 Comprar Agora
@@ -26,3 +42,49 @@ export default function Product() {
        </ProductContainer>
     )
 }
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    // Buscar os produtos mais vendidos / mais acessados
+
+    return {
+        paths: [
+            { params: {id: 'prod_Q7RPoL5kUQOXVf' } },
+            { params: {id: 'prod_Q7RM3i9xl9duAa' } },
+            { params: {id: 'prod_Q7RNoMGWLPDJQO' } },
+        ],
+        fallback: true, 
+    }
+}
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ params }) => {
+
+    if (!params || !params.id) {
+        throw new Error('Product Id is missing')
+    }
+
+    const productId = params.id
+  
+    const product = await stripe.products.retrieve(productId, {
+      expand: ['default_price']
+    })
+  
+    const price = product.default_price as Stripe.Price
+    
+    const unitAmount = price.unit_amount !== null ? price.unit_amount : 0
+  
+    return {
+      props: {
+        product: {
+          id: product.id,
+          name: product.name,
+          imageUrl: product.images[0],
+          price: new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          }).format(unitAmount / 100),
+          description: product.description
+        }
+      },
+      revalidate: 60 * 60 * 1 // 1 hours
+    }
+  }
